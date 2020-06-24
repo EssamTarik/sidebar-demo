9bc91192c806de29dbd320c57ecd1bf3ff720241
essamtarik <essamtarik2000@gmail.com>
2020-06-24 20:27:02 +0200

    Updates

---
+++ precache-manifest.fc057638048562dedf769a441f6b61f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e9f0be3c1009892e1b31c6ebec770f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3983c47aed8d8352dd7e",
+    "url": "/static/css/main.61a12902.chunk.css"
+  },
+  {
+    "revision": "dd734f065d2d2238f249",
+    "url": "/static/js/2.2cf6e85d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.2cf6e85d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3983c47aed8d8352dd7e",
+    "url": "/static/js/main.70205a2c.chunk.js"
+  },
+  {
+    "revision": "772d1be12328e6dc1572",
+    "url": "/static/js/runtime-main.add57c42.js"
+  },
+  {
+    "revision": "4f995fa49446998983e05df9994dc96c",
+    "url": "/static/media/AvenirLTStd-Medium.4f995fa4.otf"
+  },
+  {
+    "revision": "b1d7c6e085a31e9f5e4745c9aef6eb4b",
+    "url": "/static/media/AvenirLTStd-Roman.b1d7c6e0.otf"
+  }
+]);
